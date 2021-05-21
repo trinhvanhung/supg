116af7e3e18e995337db487db17a3a1b64ab74a0
trinhvanhung <trinhvanhung@gmail.com>
2021-05-21 07:07:44 +0000

    ad

---
+++ bin/CMakeFiles/supg.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/supg.dir/main.cpp.o"
+  "supg"
+  "supg.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/supg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
